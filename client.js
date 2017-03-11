a582c75b13f13e23b1ed652db17bef009fc1537a
Y Paul Sussman <ypaulsussman@gmail.com>
2017-03-10 18:42:04 -0600

    adds tickerboxes

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 var i = 1;
 var max = 18;
 var min = 1;
+var divArray = [];
 
 $(document).ready(function() {
   appendDom();
@@ -13,9 +14,31 @@ function appendDom() {
   $(".container").append("<h1>" + peopleArray[i].name + "<h1>");
   $(".container").append("<h2>" + peopleArray[i].shoutout + "<h2>");
   $(".container").append("<h2> Chiyak " + i + "/18<h2>");
-  $(".container").append("<button id='prevButton'>Prev</button>");
+  makeTickers();
+  lightTicker();
+  $(".container").append("<br><button id='prevButton'>Prev</button>");
   $(".container").append("<button id='nextButton'>Next</button>");
 }
+
+function makeTickers() {
+  for(var k = 1; k < max+1; k++){
+    $(".container").append("<div class='square noLit'></div>");
+    var $el = $(".container").children().last();
+    $el.data("id", k);
+    divArray.push($el);
+  }
+}
+
+function lightTicker() {
+  $(".square").removeClass("litUp");
+  for(var j = 0; j < divArray.length; j++){
+    if(divArray[j].data("id") == i){
+      divArray[j].removeClass("noLit");
+      divArray[j].addClass("litUp");
+    }
+  }
+}
+
 
 function addEventListener() {
   $(".container").on("click", "#nextButton", function moveNext() {
